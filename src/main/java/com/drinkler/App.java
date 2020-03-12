3f2f6813eb6a9a647b9214761119b6a73d46e4a8
Florian Drinkler <flo.drinkler@gmail.com>
2020-03-12 21:18:49 +0100

    restructured filehandler

--- src/main/java/com/drinkler/App.java
+++ src/main/java/com/drinkler/App.java
@@ -1,78 +1,22 @@
 package com.drinkler;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public final class App {
 
-    static public Graph graph;
-
-    static private int MAX_ITEMS = 100; // The input file can have a max of MAX_ITEMS lines
-    static private int MAX_IDENT = 100; // Max length of the node identifier/name
+    static private Graph graph;
+    static private Filehandler filehandler;
 
     public static void main(String[] args) {
+        filehandler = new Filehandler();
+
         try {
-            readFile("graph.txt");
+            graph = filehandler.readFile("graph.txt");
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public static void readFile(String filename) throws IOException {
-
-        checkMaxLinesInFile(filename);
-
-        File file = new File("res/" + filename);
-        FileReader fr = new FileReader(file);
-        BufferedReader br = new BufferedReader(fr);
-
-        String line;
-        while ((line = br.readLine()) != null) {
-
-            String[] splittedLine = line.trim().split(" ");
-
-            // Add Links
-            if (line.contains("-")) {
-                graph.addLinkToNode(splittedLine[0],
-                        Integer.parseInt(splittedLine[4], 0, splittedLine[4].length() - 1, 10), splittedLine[2]);
-                graph.addLinkToNode(splittedLine[2],
-                        Integer.parseInt(splittedLine[4], 0, splittedLine[4].length() - 1, 10), splittedLine[0]);
-            }
-            // Add Nodes
-            else if (line.contains("=")) {
-                graph.createNewNode(Integer.parseInt(splittedLine[2], 0, splittedLine[2].length() - 1, 10),
-                        splittedLine[0], 0);
-            }
-            // Skip comments and last line
-            else if (line.contains("//") || line.contains("}")) {
-                continue;
-            }
-            // Create Graph
-            else if (line.contains("Graph")) {
-                graph = new Graph(splittedLine[1]);
-            }
-            // Line layout couldn't be detected
-            else {
-                System.err.println("Line could not be detected!");
-                System.exit(1);
-            }
-        }
-        br.close();
 
         graph.writeKeysWithLinks();
-    }
-
-    static private void checkMaxLinesInFile(String filename) throws IOException {
-        Path path = Paths.get("res/" + filename);
-        if (Files.lines(path).count() > MAX_ITEMS) {
-            System.err.println("The input file exceeds " + MAX_ITEMS + " of lines!");
-            System.exit(2);
-        }
     }
 
 }
--- src/main/java/com/drinkler/Filehandler.java
+++ src/main/java/com/drinkler/Filehandler.java
@@ -1,95 +1,70 @@
-// package com.drinkler;
+package com.drinkler;
 
-// import java.io.BufferedReader;
-// import java.io.File;
-// import java.io.FileReader;
-// import java.io.IOException;
-// import java.nio.file.Files;
-// import java.nio.file.Path;
-// import java.nio.file.Paths;
-// import java.util.ArrayList;
-// import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-// public class Filehandler {
+public class Filehandler {
 
-// private int MAX_ITEMS = 100; // The input file can have a max of MAX_ITEMS
-// lines
-// private int MAX_IDENT = 100; // Max length of the node identifier/name
+    private int MAX_ITEMS = 100; // The input file can have a max of MAX_ITEMS lines
+    // private int MAX_IDENT = 100; // Max length of the node identifier/name
 
-// // ID of Node, and the Node itself
-// private HashMap<Integer, Node> nodes = new HashMap<Integer, Node>();
+    public Graph readFile(String filename) throws IOException {
 
-// public void readFile(String filename) throws IOException {
-// try {
-// checkMaxLinesInFile(filename);
-// } catch (IOException e) {
-// e.printStackTrace();
-// }
+        Graph tmpGraph = null;
 
-// File file = new File("res/" + filename);
-// FileReader fr = new FileReader(file);
-// BufferedReader br = new BufferedReader(fr);
+        checkMaxLinesInFile(filename);
 
-// String line;
-// while ((line = br.readLine()) != null) {
+        File file = new File("res/" + filename);
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
 
-// // Kommentarzeilen beginnen mit „//“
-// if (line.contains("//") || line.contains("{") || line.contains("}"))
-// continue;
+        String line;
+        while ((line = br.readLine()) != null) {
 
-// // Leerzeilen, führende Tabs oder Blanks werden ignoriert
-// String[] lineSplitted = line.trim().split(" ");
-// int lastElement = lineSplitted.length - 1;
-// lineSplitted[lastElement] = lineSplitted[lastElement].replace(";", "");
+            String[] splittedLine = line.trim().split(" ");
 
-// int value = Integer.parseInt(lineSplitted[lastElement]);
+            // Add Links
+            if (line.contains("-")) {
+                tmpGraph.addLinkToNode(splittedLine[0],
+                        Integer.parseInt(splittedLine[4], 0, splittedLine[4].length() - 1, 10), splittedLine[2]);
+                tmpGraph.addLinkToNode(splittedLine[2],
+                        Integer.parseInt(splittedLine[4], 0, splittedLine[4].length() - 1, 10), splittedLine[0]);
+            }
+            // Add Nodes
+            else if (line.contains("=")) {
+                tmpGraph.createNewNode(Integer.parseInt(splittedLine[2], 0, splittedLine[2].length() - 1, 10),
+                        splittedLine[0], 0);
+            }
+            // Skip comments and last line
+            else if (line.contains("//") || line.contains("}")) {
+                continue;
+            }
+            // Create Graph
+            else if (line.contains("Graph")) {
+                tmpGraph = new Graph(splittedLine[1]);
+            }
+            // Line layout couldn't be detected
+            else {
+                System.err.println("Line could not be detected!");
+                System.exit(1);
+            }
+        }
+        br.close();
 
-// // "[A-Z][A-Z,0-9]{0," + (MAX_IDENT - 1) + "}"
-// // Die Graph- bzw. Knotenbezeichner beginnen mit einem Buchstaben gefolgt von
-// // alphanumerischen Zeichen bis zu einer über die Präprozessorkonstante
-// // MAX_IDENT vorgegebenen Maximallänge
+        return tmpGraph;
+    }
 
-// // Zeilen mit Definition der Knotengewichte und Zeilen mit Definition einer
-// // Kante können bel. gemischt werden.
-// String regex = "[A-Z][A-Z,0-9]{0," + (MAX_IDENT - 1) + "}";
-// // Edges
-// if (lineSplitted[0].matches(regex) && lineSplitted[1].matches("-") &&
-// lineSplitted[2].matches(regex)) {
-// // TODO: check if node already exists, then create new node with id -1 or
-// save
-// // TODO node in link
-// // getNodeById(getNodeIdByName(lineSplitted[0])).setLink(lineSplitted[2],
-// // value);
-// // getNodeById(getNodeIdByName(lineSplitted[2])).setLink(lineSplitted[0],
-// // value);
+    private void checkMaxLinesInFile(String filename) throws IOException {
+        Path path = Paths.get("res/" + filename);
+        if (Files.lines(path).count() > MAX_ITEMS) {
+            System.err.println("The input file exceeds " + MAX_ITEMS + " of lines!");
+            System.exit(2);
+        }
+    }
 
-// }
-// // Nodes
-// else if (lineSplitted[0].matches(regex)) {
-// // TODO: check if node already exists without id, then save id or new node
-// // nodes.add(new Node(value, lineSplitted[0]));
-
-// nodes.put(value, new Node(value, lineSplitted[0]));
-// } else {
-// System.err.println("Falsch formatierte Knotenbezeichner oder Überschreitung
-// der Maximallänge von "
-// + MAX_IDENT + "!");
-// System.exit(2);
-// }
-// }
-// br.close();
-// }
-
-// /*
-// * Checks if the file doesn't exceeds MAX_ITEMS of lines
-// */
-// private void checkMaxLinesInFile(String filename) throws IOException {
-// Path path = Paths.get("res/" + filename);
-// if (Files.lines(path).count() > MAX_ITEMS) {
-// System.err.println("Die Eingabedatei hat zu viele Zeilen. Erlaubtes Maximum
-// ist " + MAX_ITEMS + "!");
-// System.exit(1);
-// }
-// }
-
-// }
+}
